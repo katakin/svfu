b2f32888bb5f75897622e0ced59381a24e0e0360
kolasss <kolas.batman@gmail.com>
2013-08-23 17:20:19 +1000

    fix utf8

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Server::Application.initialize!
 
-Encoding.default_internal = 'utf-8'
-Encoding.default_external = 'utf-8'
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
